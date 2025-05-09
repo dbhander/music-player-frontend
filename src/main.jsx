c9fffe098a1f2c2e69bae042162fe6a7aa4c3195
dgb28 <dgbhanderi20@gmail.com>
2025-05-09 00:54:15 -0400

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
 import './index.css'
 import Login from './components/Login'
 import Signup from './components/Signup'
-import Dashboard from './components/Dashboard/Dashboard';
+import Dashboard from './components/Dashboard/Dashboard'
 import PrivateRoute from './components/PrivateRoute'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <Router basename="/music-player-frontend">
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
